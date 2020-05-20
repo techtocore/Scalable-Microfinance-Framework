8d07dd75a411bf8e569eaf91889a71114ceed288
Akash Ravi <akashkravi@gmail.com>
2020-05-20 14:01:38 +0530

    Adds login/reg screens
    Signed-off-by: Akash Ravi <akashkravi@gmail.com>

--- smf-frontend/src/Login/Login.js
+++ smf-frontend/src/Login/Login.js
@@ -58,16 +58,64 @@ class Login extends Component {
         <div className="loginbtn">
           <button className="btn btn-primary" onClick={this.handleLoginClick}> Login </button>
           <br></br>
-          <button className="btn btn-primary b1"> Signup </button>
+          <button className="btn btn-primary b1" onClick={this.handleSignupClick}> Signup </button>
         </div>
       </div>
 
     );
+
+    let signup = (
+      <div>
+
+        <FontAwesomeIcon className="backicon" onClick={this.handleBackClick} icon={faArrowLeft} />
+        <h4 className="white inline">
+          Register
+            </h4>
+        <br></br>
+        <br></br>
+        <form>
+          <div className="form-row">
+            <div className="form-group col">
+              <label htmlFor="name">Full Name</label>
+              <input type="name" className="form-control" id="name" aria-describedby="emailHelp" placeholder="Enter Name" />
+            </div>
+            <div className="form-group col">
+              <label htmlFor="phoneno">Phone Number</label>
+              <input type="phone" className="form-control" id="phoneno" aria-describedby="emailHelp" placeholder="Enter Phone Number" />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="pw">Password</label>
+            <input type="password" className="form-control" id="pw" placeholder="Password" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="loc">Location</label>
+            <input type="text" className="form-control" id="loc" placeholder="Address" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="role">I am a</label>
+            <select class="custom-select">
+              <option selected>Select Role</option>
+              <option value="sender">Sender</option>
+              <option value="receiver">Receiver</option>
+              <option value="merchant">Merchant</option>
+            </select>
+          </div>
+          <br></br>
+          <button type="submit" className="btn btn-primary">Submit</button>
+        </form>
+      </div>
+    );
+
     if (opt === '') {
       return main;
     }
     else if (opt === 'login') {
       return login;
+    }
+    else if (opt === 'signup') {
+      return signup;
     }
 
   }
--- smf-frontend/src/index.js
+++ smf-frontend/src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
+import 'bootstrap/dist/js/bootstrap.js';
+import $ from 'jquery';
+import Popper from 'popper.js'
 
 ReactDOM.render(
   <React.StrictMode>
