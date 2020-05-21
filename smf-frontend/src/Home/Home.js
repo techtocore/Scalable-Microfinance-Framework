97944cb54ae647a52c8e5ed064c4ae6f3a9fd8ac
Akash Ravi <akashkravi@gmail.com>
2020-05-21 15:31:13 +0530

    Adds home content
    Signed-off-by: Akash Ravi <akashkravi@gmail.com>

--- smf-frontend/src/Home/Home.js
+++ smf-frontend/src/Home/Home.js
@@ -1,15 +1,43 @@
 import React, { Component } from 'react';
 import './Home.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link, useHistory, withRouter } from "react-router-dom";
 
 class Navbar extends Component {
-  // constructor(props){
-  // super(props);
-  // this.state = {};
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      role: '',
+      name: ''
+    };
+  }
 
   // componentWillMount(){}
-  // componentDidMount(){}
+  componentDidMount() {
+    let targetUrl = global.config.url + 'me';
+    var myHeaders = new Headers();
+    let jwt = localStorage.getItem('jwt');
+    myHeaders.append("Authorization", "JWT " + jwt);
+    let options = {
+      method: 'GET',
+      headers: myHeaders
+    };
+    fetch(targetUrl, options)
+      .then(response => {
+        console.log(response);
+        if (response.status !== 200) {
+          throw new Error(response.status)
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState({ role: data.role });
+        this.setState({ name: data.name });
+      })
+      .catch(error => {
+        console.log('error', error); this.props.history.push("/");
+      });
+  }
   // componentWillUnmount(){}
 
   // componentWillReceiveProps(){}
@@ -18,11 +46,54 @@ class Navbar extends Component {
   // componentDidUpdate(){}
 
   render(props) {
-    return (
+    let welcome = (
       <div>
-        Hii
+        <br></br>
+        <h2 className="white"> Hi {this.state.name} </h2>
+        <br></br>
       </div>
-    );
+    )
+    if (this.state.role === 'sender') {
+      return (
+        <div>
+          <ul className="list-group">
+            <li className="list-group-item list-group-item-success">Link Receiver</li>
+            <li className="list-group-item list-group-item-info">Send Money</li>
+            <li className="list-group-item list-group-item-success">View Transactions</li>
+            <li className="list-group-item list-group-item-info">Edit Profile</li>
+          </ul>
+        </div>
+      );
+    }
+    else if (this.state.role === 'receiver') {
+      return (
+        <div>
+          {welcome}
+          <ul className="list-group">
+            <li className="list-group-item list-group-item-success">Withdraw Money</li>
+            <li className="list-group-item list-group-item-info">View Transactions</li>
+            <li className="list-group-item list-group-item-success">Edit Profile</li>
+          </ul>
+        </div>
+      );
+    }
+    else if (this.state.role === 'merchant') {
+      return (
+        <div>
+          <ul className="list-group">
+            <li className="list-group-item list-group-item-success">Link Bank Account</li>
+            <li className="list-group-item list-group-item-info">View Transactions</li>
+            <li className="list-group-item list-group-item-success">Edit Profile</li>
+          </ul>
+        </div>
+      );
+    }
+    else {
+      return (
+        <span></span>
+      )
+    }
+
   }
 }
 
